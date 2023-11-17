974229896cb4aec6afc9fe2f56914cd09acdd179
JE <martinsjoseernesto@gmail.com>
2023-11-17 14:58:43 -0300

    Proper polishing for English translation, now in Release Candidate life cycle

--- settings.js
+++ settings.js
@@ -1,6 +1,5 @@
 var lang = 0;
-var ver = "v1.0.0_b2"
-//默认语言设置
+var ver = "v1.0.0.pstool_candidate.231117 (November 17, 2023)"
 check()
 function check(){
   if (navigator.language == "pt-BR") {
@@ -62,8 +61,8 @@ function seten() {
   what.innerHTML = "What is pseudolocalization?"
   uplog.innerHTML = "Update log"
   pjhome.innerHTML = "GitHub"
-  exever.innerHTML = "Windows version"
-  pstype.placeholder = "To get started, type something and click Pseudolocalize."
+  exever.innerHTML = "Windows version (x64)"
+  pstype.placeholder = "Type something to get started"
   psbtn.innerHTML = "Pseudolocalize"
   hisbtn.innerHTML = "History"
   chrbtn.innerHTML = "Character library"
@@ -79,9 +78,9 @@ function seten() {
   hashtxt.innerHTML = "[Abc12]Add a pseudo Hash ID (Resource Identifier) (a string of letters + numbers with a certain number of digits)"
   hashdit.innerHTML = "Number of digits: "
   dbvtxt.innerHTML = "Vowel repeated times: "
-  dbv1.innerHTML = "1 (Doubled vowels)"
-  nctxt.innerHTML = "Replace digits 1-9 with circled digits ①-⑨"
+  dbv1.innerHTML = "1 (Doubled vowels) (default)"
+  nctxt.innerHTML = "Replace digits 1-9 with circled digits (e.g. ①-⑨)"
   clear.innerHTML = "Clear"
-  whattxt.innerHTML = "Pseudolocalization (or pseudo-localization) is a software testing method used for testing internationalization aspects of software. Instead of translating the text of the software into a foreign language, as in the process of localization, the textual elements of an application are replaced with an altered version of the original language. <br> Besides the tools used internally by Microsoft, other internationalization tools now include pseudolocalization options. These tools include Alchemy Catalyst from Alchemy Software Development, SDL Passolo from SDL and Globalyst from g11n. Such tools include pseudo-localization capability, including ability to view rendered Pseudo-localized dialogs and automating the testing process itself. While tools like Globalyst complete the whole process of creating pseudolocalised build and automate the testing, it can also be done by running a custom made pseudolocalization script on the extracted text resources and manually testing it. <br> There are a variety of free pseudolocalization resources on the Internet that will create pseudolocalized versions of common localization formats like iOS strings, Android xml, Gettext po, and others. These sites allow developers to upload strings file to a Web site and download the resulting pseudolocalized file."
+  whattxt.innerHTML = "Pseudolocalization (or pseudo-localization) is a software testing method used for testing internationalization aspects of software. <br><br> The site is nearly in its final release. The tool is scheduled to be released in November 25, 2023."
   logtxt.innerHTML = "";
 }
